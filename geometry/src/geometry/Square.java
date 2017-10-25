3893e1fca1eb1ffd3a302b5dc9a716d46a722668
petrushka96 <33063247+petrushka96@users.noreply.github.com>
2017-10-25 20:18:04 +0300

    Add Getter and Setter

--- geometry/src/geometry/Square.java
+++ geometry/src/geometry/Square.java
@@ -4,4 +4,16 @@ package geometry;
  * Created by Administrator on 10/25/2017.
  */
 public class Square {
+       private double x;
+
+       public double SquareTerulet(){
+           return x*x;}
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
 }
