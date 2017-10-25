cf6aa21606d4a38c04d80f47d95eb132a85d91d7
Tibor <fatibor123@gmail.com>
2017-10-25 18:27:22 +0200

    Add getter sette methods

--- geometry/src/geometry/rectangle.java
+++ geometry/src/geometry/rectangle.java
@@ -1,12 +1,30 @@
 package geometry;
 
 public class rectangle {
-    Point p;
-    Point p1;
+    private Point p;
+    private Point p1;
 
     public rectangle(Point p,Point p1)
     {
         this.p = p;
         this.p1 = p1;
     }
+
+    public Point getP() {
+        return p;
+    }
+
+    public void setP(Point p) {
+        this.p = p;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public void setP1(Point p1) {
+        this.p1 = p1;
+    }
+    
+    
 }
